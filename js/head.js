c0231a4651516cb47abb3a94874b3a26ea9ca645
fightinggg <42932982+fightinggg@users.noreply.github.com>
2018-10-22 00:42:43 +0800

    Update head.js

--- js/head.js
+++ js/head.js
@@ -1,79 +1,39 @@
-* {
-    margin: 0;
-    padding: 0;
-}
- 
-body {
-    background: white;
-    position: relative;
-    height: 100%;
-    color: #777;
-    font-size: 13px;
-}
+document.writeln("<!DOCTYPE html>");
+document.writeln("<html>");
+document.writeln(" ");
+document.writeln("<head>");
+document.writeln("    <meta charset=\"utf-8\">");
+document.writeln("    <meta charset=\"utf-8\" name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0\"/>");   
+document.writeln("        <center><p style=\"color: black;\">逐梦</p></center>");
+document.writeln("    <link rel=\"stylesheet\" type=\"text/css\" href=\"/ACM/css/head.css\">");     
+document.writeln("</head>");
+document.writeln(" ");
+document.writeln("<body>");
+document.writeln("	<div class=\"out_head\">   ");
+document.writeln("		<div class='miaov_head'>");
+document.writeln("    		<ul>");
+document.writeln("        		<li><a href=\"http://fightinggg.github.io/ACM/\">计算几何</a></li>");
+document.writeln("        		<li><a href=\"http://fightinggg.github.io/ACM/\">思维</a></li>");
+document.writeln("        		<li><a href=\"http://fightinggg.github.io/ACM/\">黑科技</a></li>");
+document.writeln("        		<li><a href=\"http://fightinggg.github.io/ACM/string\">字符串</a></li>");
+document.writeln("         	    <li><a href=\"http://fightinggg.github.io/ACM/dp\">dp</a></li>");
+document.writeln("        		<li><a href=\"http://fightinggg.github.io/ACM/graph_theory/\">图论</a></li>");
+document.writeln("        		<li><a href=\"http://fightinggg.github.io/ACM/math/\">数论</a></li>");
+document.writeln("    			<li ><a href=\"http://fightinggg.github.io/ACM/data_struct/\">数据结构</a></li>");
+document.writeln("    		</ul>");
+document.writeln("   		</div>");
+document.writeln("   </div>");
 
-.out_head{
-	height: 36px;
-    width: 100%;
-    margin: 0 auto;
-    background: black;
-    margin-bottom: 0px;
-} 
- 
-li {
-    list-style: none;
-    text-decoration: none;
-}
- 
-.miaov_head {
-    height: 36px;
-    width: 100%;
-    margin: 0 auto;
-    background: black;
-    margin-top: 12px;
-}
- 
- 
-.miaov_head ul {
-    float: left;
-    width: 95%;
-    height: 36px;
-    margin-top: 30px;
-    color: white;
-    position: absolute;
-    top: 0px;
-    margin-left: 0px;
-}
- 
-.miaov_head ul li {
-	position: relative;
-    float: right;
-    margin-left: 2%;
-    color: white;
-    list-style: none;
-}
- 
-.miaov_head ul li a {
-    color: white;
-    font-size: 14px;
-    text-decoration: none;
-}
- 
-.miaov_head input {
-    position: absolute;
-    top: 5px;
-    margin-left: 1000px;
-    width: 200px;
-    height: 22px;
-}
- 
-.miaov_head a {
-    line-height: 36px;
-    color: #777;
-}
- 
-.miaov_head a:hover {
-    color: #555;
-}
+document.writeln(" 	<center>");
+document.writeln("	<div class=\"out_sign\">");
+document.writeln("	<div class=\"sign\">");
+document.writeln(" 		</br></br></br></br></br></br></br>");
+document.writeln(" 			相</br>信</br>不</br>屈</br>不</br>挠</br>的</br>努</br>力</br>");
+document.writeln(" 			</br></br>");
+document.writeln("		相</br>信</br>战</br>胜</br>死</br>亡</br>的</br>年</br>轻</br>");
+document.writeln("	</div>");
+document.writeln("	</div>");
+document.writeln("	</center>");
 
 
 
@@ -82,25 +42,10 @@ li {
 
 
 
+			
 
 
 
-
-
-
- .out_sign{
- 	float:left;
-	margin-top: 50px;
-    height:600px;
-    width: 15%;
-    background: white;
- }
- 
- 
- 
-.sign {
-    height:600px;
-    width: 30px;
-    background: black;
-     color: white;
-}
+document.writeln("</body>");
+document.writeln(" ");
+document.writeln("</html>");
