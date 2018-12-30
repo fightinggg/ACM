9adc700f23c0d63335a78be786b603697521740b
fightinggg <1144560553@qq.com>
2018-12-31 01:42:30 +0800

    11

--- js/up_and_right.js
+++ js/up_and_right.js
@@ -1,6 +1,6 @@
  document.writeln("   <div id=\"up\">  <center><a href=\"http://fightinggg.github.io/ACM/\">逐    梦</a></center></div>");
 document.writeln("  		     	<div id=\"right\">");
-document.writeln("  		     		    -------------------------------------");
+document.writeln("  		     		    ---------------------------------");
  document.writeln("    					<h3><center>ACM</center></h3>");
  document.writeln("    					    						<a href=\"http://fightinggg.github.io/ACM/math/\">数论</a></br>");
  document.writeln("    					    						<a href=\"http://fightinggg.github.io/ACM/data_struct/\">数据结构</a></br>");
@@ -11,5 +11,5 @@ document.writeln("  		     		    -------------------------------------");
  document.writeln("    											<a href=\"http://fightinggg.github.io/ACM/thinging\">思维与算法</a></br>");
 	 document.writeln("    										<a href=\"http://fightinggg.github.io/ACM/Yuki_technique/\">黑科技</a></br>");
 	 document.writeln("    										<a href=\"http://fightinggg.github.io/ACM/acm_head_file.html\">快读</a></br>");
-   document.writeln("    					             			-------------------------------------");
+   document.writeln("    					             			---------------------------------");
    document.writeln("    					</div>");
