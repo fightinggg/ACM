5f57e6cc2f419e9c371892203c91c5a284651ed6
fightinggg <1144560553@qq.com>
2018-12-30 22:34:58 +0800

    ??
    ??

---
+++ js/up_and_right.js
@@ -0,0 +1,16 @@
+ document.writeln("   <div id=\"up\">  <center>逐    梦</center></div>");
+		     
+document.writeln("  		     	<div id=\"right\">");
+document.writeln("  		     		    -------------------------------------");
+ document.writeln("    					<h3><center>ACM</center></h3>");
+ document.writeln("    					    						<a href=\"http://fightinggg.github.io/ACM/math/\">数论</a></br>");
+ document.writeln("    					    						<a href=\"http://fightinggg.github.io/ACM/data_struct/\">数据结构</a></br>");
+  document.writeln("    					   						<a href=\"http://fightinggg.github.io/ACM/string\">字符串</a></br>");
+ document.writeln("    											<a href=\"http://fightinggg.github.io/ACM/dp\">dp</a></br>");
+ document.writeln("    											<a href=\"http://fightinggg.github.io/ACM/graph_theory/\">图论</a></br>");
+ document.writeln("    											<a href=\"http://fightinggg.github.io/ACM/computational_geometry\">计算几何</a></br>");
+ document.writeln("    											<a href=\"http://fightinggg.github.io/ACM/thinging\">思维与算法</a></br>");
+	 document.writeln("    										<a href=\"http://fightinggg.github.io/ACM/Yuki_technique/\">黑科技</a></br>");
+	 document.writeln("    										<a href=\"http://fightinggg.github.io/ACM/acm_head_file.html\">快读</a></br>");
+   document.writeln("    					             			-------------------------------------");
+   document.writeln("    					</div>");
