b77d4c1d48ed952a5ae166cdf3f4041f447fdce9
fightinggg <1144560553@qq.com>
2019-01-02 01:12:52 +0800

    s
    a

--- js/link.js
+++ js/link.js
@@ -3,7 +3,8 @@
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/pastebin.css\">");
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/code.css\">");
    
- document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 1000px)\" href=\"/ACM/css/style.css\">");
+ document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 1000px)\" href=\"/ACM/css/menu_style_phone.css\">");
+ document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 1000px)\" href=\"/ACM/css/menu_style.css\">");
  document.writeln("   <script type=\"text/javascript\" src=\"/ACM/js/jquery.min.js\"></script>");
  document.writeln("    <script type=\"text/javascript\" src=\"/ACM/js/menu.js\"></script>");
  
