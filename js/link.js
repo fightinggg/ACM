c9215d62069153f1ec424c12ecf1f81d5de548a2
fightinggg <1144560553@qq.com>
2019-01-01 20:47:18 +0800

    asdf
    asdf

--- js/link.js
+++ js/link.js
@@ -1,5 +1,5 @@
-   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 1200px)\" href=\"/ACM/css/head_mobile.css\">");
-   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 1200px)\" href=\"/ACM/css/head.css\">");
+   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 1000px)\" href=\"/ACM/css/head_mobile.css\">");
+   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 1000px)\" href=\"/ACM/css/head.css\">");
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/pastebin.css\">");
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/code.css\">");
    
