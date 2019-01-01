497cd44d2248ea8e65fee3ef065e317e590394c3
fightinggg <1144560553@qq.com>
2019-01-01 23:18:05 +0800

    asdf
    asdf

--- js/link.js
+++ js/link.js
@@ -3,3 +3,11 @@
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/pastebin.css\">");
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/code.css\">");
    
+ document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 1000px)\" href=\"/ACM/css/style.css\">");
+ document.writeln("   <script type=\"text/javascript\" src=\"/ACM/js/jquery.min.js\"></script>");
+ document.writeln("    <script type=\"text/javascript\" src=\"/ACM/js/menu.js\"></script>");
+ 
+
+
+
+   
