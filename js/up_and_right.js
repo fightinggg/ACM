7a0dee4197238406727b164f678fece9f73f7075
fightinggg <1144560553@qq.com>
2019-01-02 17:23:35 +0800

    adf
    asdf

--- js/up_and_right.js
+++ js/up_and_right.js
@@ -3,6 +3,8 @@
 
 document.writeln("  		     	<nav id=\"menu\" class=\"menu\">     ");
 		document.writeln("  		     	<div id=\"right\">");
+		
+		
 		/*1111111111111111111111*/
 		document.writeln("  		     	<div id=\"right_in\">");
  		document.writeln("    					<h3><center>ACM模版</center></h3>");
@@ -18,6 +20,8 @@ document.writeln("  		     	<nav id=\"menu\" class=\"menu\">     ");
 		document.writeln("    										    <a href=\"http://fightinggg.github.io/ACM/stencil/acm_head_file.html\">快读</a></br>");
  		document.writeln("    					<hr>");
  		document.writeln("    					</div>");
+ 		
+ 		
 		/*22222222222222222222*/ 
 		document.writeln("  		     	<div id=\"right_in\">");
  		document.writeln("    					<h3><center>ACM题型</center></h3>");
@@ -32,6 +36,26 @@ document.writeln("  		     	<nav id=\"menu\" class=\"menu\">     ");
 		document.writeln("    										    <a href=\"http://fightinggg.github.io/ACM/problem/Yuki_technique/\">黑科技</a></br>");
 		document.writeln("    					<hr>");
 		document.writeln("    					</div>");
+		
+		
+		document.writeln("  		     	<div id=\"right_in\">");
+ 		document.writeln("    					<h3><center>珍藏网站</center></h3>");
+ 		document.writeln("    					<hr>");
+ 		document.writeln("    					    						<a href=\"https://www.codecogs.com/latex/eqneditor.php\" target=\"_blank\">数学公式</a></br>");
+ 		document.writeln("    					    						<a href=\"http://hzwer.com\" target=\"_blank\">hzwer</a></br>");
+		document.writeln("    					<hr>");
+		document.writeln("    					</div>");
+		
+		
+		document.writeln("  		     	<div id=\"right_in\">");
+ 		document.writeln("    					<h3><center>友链</center></h3>");
+ 		document.writeln("    					<hr>");
+ 		document.writeln("    					    						<a href=\"https://blog.csdn.net/weixin_41863129\" target=\"_blank\">yg</a></br>");
+ 		document.writeln("    					    						<a href=\"https://decaku.github.io\">zwg</a></br>");
+		document.writeln("    					<hr>");
+		document.writeln("    					</div>");
+		
+		
 		document.writeln("    					</div>");
  		document.writeln("    					 </nav><main id=\"main\" class=\"panel\"><button class=\"btn-hamburger js-slideout-toggle\"><span class=\"tooltip\">目录</span></button></main><script type=\"text/javascript\" src=\"/ACM/js/slideout.min.js\"></script><script type=\"text/javascript\">var slideout = new Slideout({'panel': document.getElementById('main'),'menu': document.getElementById('menu'),'padding': 256,'tolerance': 70});document.querySelector('.js-slideout-toggle').addEventListener('click', function() {slideout.toggle(); });document.querySelector('.menu').addEventListener('click', function(eve) {if (eve.target.nodeName === 'A') { slideout.close(); }});function aload(t){\"use strict\";t=t||window.document.querySelectorAll(\"[data-aload]\"),void 0===t.length&&(t=[t]);var a,e=0,r=t.length;for(e;r>e;e+=1)a=t[e],a[\"LINK\"!==a.tagName?\"src\":\"href\"]=a.getAttribute(\"data-aload\"),a.removeAttribute(\"data-aload\");return t}window.onload = function(){aload();document.querySelector('.iphone').className += ' shown';}</script>");
 
