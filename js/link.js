9650a170cbc894dd875d15b7ba433a4087dbd6b9
fightinggg <1144560553@qq.com>
2019-01-02 17:42:43 +0800

    asdf
    adf

--- js/link.js
+++ js/link.js
@@ -1,7 +1,7 @@
-   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 795px)\" href=\"/ACM/css/head_mobile.css\">");
-   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 795px)\" href=\"/ACM/css/head.css\">");
+   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 800px)\" href=\"/ACM/css/head_mobile.css\">");
+   document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 800px)\" href=\"/ACM/css/head.css\">");
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/pastebin.css\">");
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/code.css\">");
    
- document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 795px)\" href=\"/ACM/css/menu_style_phone.css\">");
- document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 795px)\" href=\"/ACM/css/menu_style.css\">");
+ document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 800px)\" href=\"/ACM/css/menu_style_phone.css\">");
+ document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 800px)\" href=\"/ACM/css/menu_style.css\">");
