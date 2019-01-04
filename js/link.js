99e987b0d2ff51fbfa6dfd4e1c26f818f6087f8d
fightinggg <1144560553@qq.com>
2019-01-04 23:00:05 +0800

    asdf
    asdf

--- js/link.js
+++ js/link.js
@@ -5,4 +5,6 @@
    
  document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 999px)\" href=\"/ACM/css/menu_style_phone.css\">");
  document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 1000px)\" href=\"/ACM/css/menu_style.css\">");
+
+//自动提交
 document.writeln("    <script>(function(){var bp = document.createElement('script');var curProtocol = window.location.protocol.split(':')[0];if (curProtocol === 'https'){bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';}else{bp.src = 'http://push.zhanzhang.baidu.com/push.js';}var s = document.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(bp, s);})();</script>")
