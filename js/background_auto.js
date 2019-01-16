91cd0feb4d4546d04c6e5848070ece26f7b4b174
fightinggg <1144560553@qq.com>
2019-01-16 15:46:27 +0800

    2
    2

---
+++ js/background_auto.js
@@ -0,0 +1,2 @@
+  var randomBgIndex = Math.round( Math.random() * 4 );
+  document.write('<style>body{background:url(/ACM/img/' + randomBgIndex + '.jpg) !important;}</style>');
--- js/link.js
+++ js/link.js
@@ -4,7 +4,18 @@
    document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/css/code.css\">");
    
  document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (max-width: 999px)\" href=\"/ACM/css/menu_style_phone.css\">");
- document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 1000px)\" href=\"/ACM/css/menu_style.css\">");
+document.writeln("   <link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"screen and (min-width: 1000px)\" href=\"/ACM/css/menu_style.css\">");
+
+
 
 //自动提交
 document.writeln("    <script>(function(){var bp = document.createElement('script');var curProtocol = window.location.protocol.split(':')[0];if (curProtocol === 'https'){bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';}else{bp.src = 'http://push.zhanzhang.baidu.com/push.js';}var s = document.getElementsByTagName(\"script\")[0];s.parentNode.insertBefore(bp, s);})();</script>")
+
+
+
+
+document.writeln("   <link rel=\"stylesheet\" type=\"text/css\"  href=\"/ACM/js/background_auto.js\">");
+
+
+
+
