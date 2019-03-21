8b19354df06b38621e9f76d487831e27a66ae635
fightinggg <42932982+fightinggg@users.noreply.github.com>
2019-03-21 21:59:45 +0800

    asdf
    asdf

--- js/web_end.js
+++ js/web_end.js
@@ -1,7 +1,7 @@
 
 document.writeln('                    </div>																																																	');
 document.writeln('                    </div>																																																	');
-document.writeln('                    <div id="down"><center>all right</center><center>本人维护</center></div>																																																');
+document.writeln('                    <div id="down"><center>all right</center><center>本人维护</center><center><div style="width:80%"><hr></div></center><br></div>																																																');
 document.writeln('                    </div>																																																	');
 document.writeln('                    </body>																																																	');
 
