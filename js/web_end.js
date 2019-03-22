7df321a871b525799ddca0effb634be371e29e3d
fightinggg <42932982+fightinggg@users.noreply.github.com>
2019-03-22 20:13:05 +0800

    a
    a

--- js/web_end.js
+++ js/web_end.js
@@ -1,7 +1,7 @@
 
 document.writeln('                    </div>																																																	');
 document.writeln('                    </div>																																																	');
-document.writeln('                    <div id="down"><center>all right</center><center>本人维护</center><center><div style="width:80%"><hr></div></center><br></div>																																																');
+document.writeln('                    <div id="down"><center>all right</center><center>如有问题请联系qq:1144560553</center><center><div style="width:80%"><hr></div></center><br></div>																																																');
 document.writeln('                    </div>																																																	');
 document.writeln('                    </body>																																																	');
 
